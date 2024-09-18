c6dc26f97b6c520aa204355011d43da023401159
NLxDoDge <nlxdodge@gmail.com>
2024-09-18 15:37:34 +0200

    feat: make different scripts in a new script directory

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,5 +1,3 @@
-git pull
-docker network inspect global >/dev/null 2>&1
 docker-compose -f "traefik/docker-compose.yml" down
 docker-compose -f "portainer/docker-compose.yml" down
 docker-compose -f "jellyfin/docker-compose.yml" down
---
+++ scripts/setup.sh
@@ -0,0 +1,2 @@
+git pull
+docker network inspect global >/dev/null 2>&1
