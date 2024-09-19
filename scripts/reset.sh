8924ddce92cd64dddf4823e870ad8afdcd8ba547
NLxDoDge <nlxdodge@gmail.com>
2024-09-19 09:51:21 +0200

    fix: folder issue

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,9 +1,9 @@
-docker-compose -f "traefik/docker-compose.yml" down
-docker-compose -f "portainer/docker-compose.yml" down
-docker-compose -f "jellyfin/docker-compose.yml" down
-docker-compose -f "scrutiny/docker-compose.yml" down
+docker-compose -f "../traefik/docker-compose.yml" down
+docker-compose -f "../portainer/docker-compose.yml" down
+docker-compose -f "../jellyfin/docker-compose.yml" down
+docker-compose -f "../scrutiny/docker-compose.yml" down
 
-docker-compose -f "portainer/docker-compose.yml" up -d
-docker-compose -f "traefik/docker-compose.yml" up -d
-docker-compose -f "jellyfin/docker-compose.yml" up -d
-docker-compose -f "scrutiny/docker-compose.yml" up -d
+docker-compose -f "../portainer/docker-compose.yml" up -d
+docker-compose -f "../traefik/docker-compose.yml" up -d
+docker-compose -f "../jellyfin/docker-compose.yml" up -d
+docker-compose -f "../scrutiny/docker-compose.yml" up -d
