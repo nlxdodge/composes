859ee2ec5bc70ad9ccc96debb2cb6c5c6841e908
NLxDoDge <nlxdodge@gmail.com>
2024-09-19 14:24:05 +0200

    fix: always pull latest images

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,7 +3,7 @@ docker-compose -f "../portainer/docker-compose.yml" down
 docker-compose -f "../jellyfin/docker-compose.yml" down
 docker-compose -f "../scrutiny/docker-compose.yml" down
 
-docker-compose -f "../portainer/docker-compose.yml" up -d
-docker-compose -f "../traefik/docker-compose.yml" up -d
-docker-compose -f "../jellyfin/docker-compose.yml" up -d
-docker-compose -f "../scrutiny/docker-compose.yml" up -d
+docker-compose -f "../portainer/docker-compose.yml" up -d --pull always
+docker-compose -f "../traefik/docker-compose.yml" up -d --pull always
+docker-compose -f "../jellyfin/docker-compose.yml" up -d --pull always
+docker-compose -f "../scrutiny/docker-compose.yml" up -d --pull always
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
-docker-compose -f "../portainer/docker-compose.yml" up -d
-docker-compose -f "../traefik/docker-compose.yml" up -d
-docker-compose -f "../jellyfin/docker-compose.yml" up -d
-docker-compose -f "../scrutiny/docker-compose.yml" up -d
+docker-compose -f "../portainer/docker-compose.yml" up -d --pull always
+docker-compose -f "../traefik/docker-compose.yml" up -d --pull always
+docker-compose -f "../jellyfin/docker-compose.yml" up -d --pull always
+docker-compose -f "../scrutiny/docker-compose.yml" up -d --pull always
