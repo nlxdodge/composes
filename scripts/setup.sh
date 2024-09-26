29410354d9d9762d1bf717ced7f04e9574a943d9
NLxDoDge <nlxdodge@gmail.com>
2024-09-26 16:32:02 +0200

    fix: folder bug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ docker network inspect global >/dev/null 2>&1
 
 ## setup and copy over rtorrent confiruration
 mkdir -p ~/configurations/rtorrent
-cp ./jellyfin/rtorrent.rc ~/configurations/rtorrent/rtorrent.rc
+cp ../jellyfin/rtorrent.rc ~/configurations/rtorrent/rtorrent.rc
