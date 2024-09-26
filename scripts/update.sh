0488cdd7ffece124b56ddd4c83e10702eb6eb09c
NLxDoDge <nlxdodge@gmail.com>
2024-09-26 16:39:16 +0200

    ci: for now create update script and lower work in reset script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 git pull
 
-services=("jellyfin")
+services=("traefik" "portainer" "scrutiny" "jellyfin")
 
 for service in "${services[@]}"; do
   cd "../$service/"
   pushd .
   docker-compose down
+  # docker-compose pull
   docker-compose up -d
   popd
 done
