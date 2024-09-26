6e35fba52a65577c1f60d8a902d103432a9d2999
NLxDoDge <nlxdodge@gmail.com>
2024-09-26 16:31:23 +0200

    feat: create separate config for rtorrent

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,7 @@
+## update repository and create global network for all containers to share if needed
 git pull
 docker network inspect global >/dev/null 2>&1
+
+## setup and copy over rtorrent confiruration
+mkdir -p ~/configurations/rtorrent
+cp ./jellyfin/rtorrent.rc ~/configurations/rtorrent/rtorrent.rc
