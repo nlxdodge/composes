2a7c41714f86b0d857af1d7e17fb205018b9565e
NLxDoDge <nlxdodge@gmail.com>
2025-03-14 14:25:11 +0100

    ci: add portfolio to setup and compose

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,6 @@ docker network inspect global >/dev/null 2>&1
 ## setup and copy over rtorrent confiruration
 mkdir -p ~/configurations/rtorrent
 mkdir -p ~/servers/bedrock
+
+# clone portfolio repository
+git clone https://github.com/nlxdodge/portfolio-astro.git ~/portfolio-astro
