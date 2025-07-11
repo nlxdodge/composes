e55177d94cb70851340d3c2c2548cb95546ca1df
NLxDoDge <nlxdodge@gmail.com>
2025-07-11 15:42:35 +0200

    feat: make remove possible of portfolio container and image

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -2,9 +2,15 @@
 source helpers/pull.sh
 source helpers/services.sh
 
+if "portfolio" in "${services[@]}"; do
+  docker rm portfolio
+  docker image remove portfolio_portfolio
+done
+
 for service in "${services[@]}"; do
   cd /home/nlxdodge/composes/$service/
   pushd .
   docker-compose down
   popd
 done
+
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,6 +2,11 @@
 source helpers/pull.sh
 source helpers/services.sh
 
+if "portfolio" in "${services[@]}"; do
+  docker rm portfolio
+  docker image remove portfolio_portfolio
+done
+
 for service in "${services[@]}"; do
   cd /home/nlxdodge/composes/$service/
   pushd .
