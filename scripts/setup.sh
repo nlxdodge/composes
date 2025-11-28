fb2bd50dd0c9702c4a6dd2106c9e20e4708ce373
NLxDoDge <nlxdodge@gmail.com>
2025-11-28 13:35:02 +0100

    wip: ipv6 for docker

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,13 @@
 source pull.sh
 
-echo "Creating global network"
-docker network create global >/dev/null 2>&1 || true
+echo "Creating global network for IPv4 and IPv6"
+DAEMON_CONFIG = '{
+  "ipv6": true,
+  "fixed-cidr-v6": "2001:db8:1::/64"
+}'
+echo "$DAEMON_CONFIG" | sudo tee /etc/docker/daemon.json > /dev/null
+
+docker network create --ipv6 global> /dev/null 2>&1 || true
 
 echo "Creating folders"
 mkdir -p /home/nlxdodge/servers/bedrock
